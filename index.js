a2e8ca9dddf1514fc4f83cc2a8e97d85123755d4
Jon Church <me@jonchurch.com>
2020-01-15 21:04:48 -0500

    create basic server w/ multer upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const multer = require('multer');
+
+const upload = multer({ dest: 'files/' });
+
+const app = express();
+
+app.post('/file', upload.single('file'), (req, res, next) => {
+  console.log(req.file);
+  res.send(req.file);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
