ffb682cecfced1613f2c208a07e1bf7c5ab77a1a
Jon Church <me@jonchurch.com>
2020-03-02 23:19:51 -0500

    create example with multer 2.0.0-rc.1

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
+const fs = require('fs');
+const { promisify } = require('util');
+const pipeline = promisify(require('stream').pipeline);
+
 const express = require('express');
 const multer = require('multer');
 
-const upload = multer({ dest: 'files/' });
+const upload = multer();
 
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.post('/file', upload.single('file'), (req, res, next) => {
-  console.log(req.file);
-  res.send(req.file);
+async function createRecord(obj) {
+  return { id: Math.floor(Math.random() * 1000), shipName: obj.shipName };
+}
+
+app.post('/file', upload.single('avatar'), async (req, res, next) => {
+  try {
+    const { shipName, ...otherFields } = req.body;
+    const { file } = req;
+    const record = await createRecord({ shipName, ...otherFields });
+
+    const finalFileName = `${record.id}-${shipName}${file.detectedFileExtension}`;
+    await pipeline(
+      file.stream,
+      fs.createWriteStream(`${__dirname}/files/${finalFileName}`)
+    );
+    res.send(`File uploaded as ${finalFileName}`);
+  } catch (err) {
+    console.log('File upload failed', err);
+    next(err);
+  }
 });
 
 const PORT = process.env.PORT || 3000;
